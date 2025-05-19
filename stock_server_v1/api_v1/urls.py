4d15cf4162fadcf2c48e320427a530605acdca47
LUO WEN SHIN <wadevs14161@gmail.com>
2025-05-19 20:44:57 +0100

    basic templates

--- stock_server_v1/api_v1/urls.py
+++ stock_server_v1/api_v1/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import StockPriceHistoryViewSet
+from .views import StockPriceHistoryViewSet, index
 
 router = routers.DefaultRouter()
 router.register(r'stock-price-history', StockPriceHistoryViewSet, basename='stock-price-history')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('', index, name='index'),
 ]
 
 
